f9fb88ece671443ec763c288a4acee72091a3b00
sagar.madala <sagar.madala@rakuten.com>
2022-05-20 23:48:31 +0530

    fileop: rename files

--- file/test/fileparsertest.cpp
+++
@@ -1,9 +0,0 @@
-#include "fileparser.h"
-
-int main(int charc, char* argv[]) {
-	std::string filePath = argv[1];
-	std::ifstream stream(filePath);
-	for (auto& line: cool::IteratorRange(stream)) {
-		std::cout << *line << std::endl;
-	}
-}
